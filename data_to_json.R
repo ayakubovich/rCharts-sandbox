fb30439c63d29e113cccdca90097590a68957905
Alex Yakubovich <alex.yakubovich@gmail.com>
2015-02-24 00:05:20 -0500

    add documentation, a la roxygen

--- data_to_json.R
+++ data_to_json.R
@@ -1,3 +1,22 @@
+#'@title 
+#'Data To JSON
+#'
+#'@description 
+#'This function convert data to JSON format
+#'
+#'@details
+#'This function converts data to JSON format that can be used in Shiny and Highcharts
+#'
+#'@param data data.frame that contains data
+#'@param col_to_json columns that should be converted to JSON
+#'@param col_names names of the columns
+#'
+#'@examples
+#'data <- data.frame(one = runif(5), two = runif(5))
+#'data <- to_json(data, col_to_json=c('one', 'two'))
+#'
+#'@export
+
 data_to_json <- function(data, col_to_json=NULL, col_names=NULL) {
   
   if(!is.null(col_to_json)) {
